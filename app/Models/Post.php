ee2eb9f4e73074ff4dcc793dff11090c9daa3cd9
Chihiro890 <chihiro262001@yahoo.co.jp>
2022-09-24 16:20:24 +0900

    一覧画面

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,10 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage;
+// Strageをuse文で読み込む
 
 class Post extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'body',
+    ];
 
     /**
      * Get the user that owns the Post
@@ -18,4 +25,15 @@ class Post extends Model
     {
         return $this->belongsTo(User::class);
     }
+    // public function image_url()
+    // {
+    //     return Storage::url('images/posts/' . $this->image);
+    // }
+
+    public function getImageUrlAttribute()
+    {
+        return Storage::url('images/posts/' . $this->image);
+    }
 }
+
+//     if (!Storage::putFileAs('images/posts', $file, $post->image)) {
